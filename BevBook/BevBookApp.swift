31dd5419ba795d1f0834aac81e47db5433ba7301
Tyson Brown <tysonbrown104@icloud.com>
2025-01-26 16:48:06 -0500

    Initial Commit

---
+++ BevBook/BevBookApp.swift
@@ -0,0 +1,17 @@
+//
+//  BevBookApp.swift
+//  BevBook
+//
+//  Created by Tyson Brown on 1/26/25.
+//
+
+import SwiftUI
+
+@main
+struct BevBookApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ BevBookTests/BevBookTests.swift
@@ -0,0 +1,17 @@
+//
+//  BevBookTests.swift
+//  BevBookTests
+//
+//  Created by Tyson Brown on 1/26/25.
+//
+
+import Testing
+@testable import BevBook
+
+struct BevBookTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
